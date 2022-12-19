144ebd894bf97cd468ea0d0498558735e4b1bdb7
ahliddinjoraboyev553 <114213637+ahliddinjoraboyev553@users.noreply.github.com>
2022-12-19 12:24:25 +0500

    Add files via upload

---
+++ 2-Amaliy ish.py
@@ -0,0 +1,9 @@
+a=int(input('a='))
+b=int(input('b='))
+c=int(input('c='))
+
+V=a*b*c
+S=2*(a*b+b*c+a*c)
+
+print(f'paralelopipedning hajmi {V} ga teng')
+print(f'paralelopipedning yuzasi {S} ga teng')
